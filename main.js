f0ba625046d8273f7722db4fd2995644e08a5a7f
Mahnoor <muhammad31zayyan@gmail.com>
2024-12-08 18:35:24 +0500

    JS Assigment 1

---
+++ main.js
@@ -0,0 +1,30 @@
+//  parsefloat practice
+
+ const firstnumber = prompt('what is first number');
+  console.log('first number' , firstnumber)
+
+ const number = prompt('what is second number');
+ console.log('second number' ,number)
+
+  parseFloat(alert(`Your result is ${firstnumber + number}`))
+
+//   how to give total amount with discount
+
+
+ const name = prompt('what is your name?');
+  console.log('Name' , name)
+
+  const amount = prompt('what is your total amount?');
+  console.log('amount' ,amount)
+
+//  alert(`Your Total amount with 10% discount  is ${purchaseAmount * discount / 100 , finalAmount = purchaseAmount - discountAmount}))
+parseFloat(alert(`Your Total amount with 10% discount  is ${amount * 10 / 100} finalAmount ${amount - 10}`))
+
+
+// talk with user
+const animalname = prompt('What Is Your Favourite Animal Name?');
+ console.log('Animal Name' , animalname)
+
+ alert (`Wow! ${animalname} are amazing creatures`)
+
+
